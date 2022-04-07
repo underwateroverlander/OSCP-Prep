3cb8f8e5181885d5a7516ea1a6ba05ff6e2301c0
Zach Bennefield <zbennefield@tenable.com>
2022-04-07 14:37:16 -0400

    Consolidate functions
    Consolidated multiple lines to functions.

--- Scripts/enumeration.py
+++ Scripts/enumeration.py
@@ -7,15 +7,12 @@ from sys import exit
 #Am I running with root priv?
 if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again using 'su -' or 'sudo'. Exiting.")
+#Upgrade distro. May want to disable for exam.
+upgradedist = "apt update; apt dist-upgrade; apt autoremove; apt clean"
+upgraderet = subprocess.run(upgradedist, shell=True)
 #Grab some necessary files
-subprocess.run(['apt update'])
-subprocess.run(['apt dist-upgrade'])
-subprocess.run(['apt autoremove'])
-subprocess.run(['apt clean'])
-subprocess.run(['apt install kali-tools-top10 man-db exploitdb'], shell=True)
-subprocess.run(['apt install python3 seclists curl enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf wmshutdown'], shell=True)
-subprocess.run(['apt install python3-pip'], shell=True)
-
+toolinstall = "apt install man-db exploitdb; apt install python3 seclists curl enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf; apt install python3-pip"
+subprocess.run(toolinstall, shell=True)
 #Let's make sure there is an IP file
 while True:
    ipask = input('Do you have a file named iplist.txt?')
@@ -25,9 +22,9 @@ while True:
    elif ipask.lower().startswith("y"):
       print("Perfect!")
       break
-      
+
 #Make it easier to use IP list
-def ip():
+def iplist():
     with open('iplist.txt', 'r') as f:
         iplist = f.read().replace('\n', '')
 #Let's grab AutoRecon and store it for use. We are going to make an assumption Kali is being used for OSCP and install some packages
